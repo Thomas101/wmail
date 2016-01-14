2cb87cbb653b52fecfafa6d7ebefe52b36eb480e
Thomas Beverley <tom.beverley101@gmail.com>
2016-01-14 17:51:23 +0000

    moved app menu into its own file

---
+++ src/appMenu.js
@@ -0,0 +1,56 @@
+const Menu = require('menu')
+
+module.exports = {
+	/**
+	* @param selectors: the selectors for the non-standard actions
+	* @return the menu
+	*/
+	build : function(selectors) {
+	  return Menu.buildFromTemplate([
+	    {
+	      label: "Application",
+	      submenu: [
+	        { label: "About", selector: "orderFrontStandardAboutPanel:" },
+	        { type: "separator" },
+	        { label: "Quit", accelerator: "Command+Q", click: selectors.fullQuit }
+	      ]
+	    },
+	    {
+	      label: "Edit",
+	      submenu: [
+	        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+	        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+	        { type: "separator" },
+	        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+	        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+	        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+	        { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+	      ]
+	    },
+	    {
+	      label: 'View',
+	      submenu: [
+	        { label: 'Toggle Full Screen', accelerator: 'Ctrl+Command+F', click: selectors.fullscreenToggle },
+	        { type: "separator" },
+	        { label: "Reload", accelerator: "CmdOrCtrl+R", click: selectors.reload },
+	        { label: "Developer Tools", accelerator: "Alt+CmdOrCtrl+J", click: selectors.devTools }
+	      ]
+	    },
+	    {
+	      label: 'Window',
+	      role: 'window',
+	      submenu: [
+	        { label: 'Minimize', accelerator: 'CmdOrCtrl+M', role: 'minimize' }
+	      ]
+	    },
+	    {
+	      label: 'Help',
+	      role: 'help',
+	      submenu: [
+	        { label: 'Project Homepage', click: selectors.learnMore },
+	        { label: 'Report a Bug', click: selectors.bugReport }
+	      ]
+	    },
+	  ]);
+	}
+}
--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ const GoogleAuthServer = require('./auth/GoogleAuthServer')
 const CONSTANTS = require('./constants')
 const update = require('./update')
 const analytics = require('./analytics')
+const appMenu = require('./appMenu')
 
 let mailboxWindow
 let googleAuth
@@ -23,63 +24,6 @@ app.on('window-all-closed', function() {
 app.on('activate', function(){
   if (mailboxWindow) { mailboxWindow.show() }
 });
-
-/*****************************************************************************/
-// Menu
-/*****************************************************************************/
-
-/**
-* @param selectors: the selectors for the non-standard actions
-* @return the menu
-*/
-function buildMenu(selectors) {
-  return Menu.buildFromTemplate([
-    {
-      label: "Application",
-      submenu: [
-        { label: "About", selector: "orderFrontStandardAboutPanel:" },
-        { type: "separator" },
-        { label: "Quit", accelerator: "Command+Q", click: selectors.fullQuit }
-      ]
-    },
-    {
-      label: "Edit",
-      submenu: [
-        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
-        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
-        { type: "separator" },
-        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
-        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
-        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
-        { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
-      ]
-    },
-    {
-      label: 'View',
-      submenu: [
-        { label: 'Toggle Full Screen', accelerator: 'Ctrl+Command+F', click: selectors.fullscreenToggle },
-        { type: "separator" },
-        { label: "Reload", accelerator: "CmdOrCtrl+R", click: selectors.reload },
-        { label: "Developer Tools", accelerator: "Alt+CmdOrCtrl+J", click: selectors.devTools }
-      ]
-    },
-    {
-      label: 'Window',
-      role: 'window',
-      submenu: [
-        { label: 'Minimize', accelerator: 'CmdOrCtrl+M', role: 'minimize' }
-      ]
-    },
-    {
-      label: 'Help',
-      role: 'help',
-      submenu: [
-        { label: 'Project Homepage', click: selectors.learnMore },
-        { label: 'Report a Bug', click: selectors.bugReport }
-      ]
-    },
-  ]);
-}
 
 
 /*****************************************************************************/
@@ -102,7 +46,7 @@ app.on('ready', function() {
   googleAuth = new GoogleAuthServer(mailboxWindow.webContents)
 
   // Setup the menu
-  Menu.setApplicationMenu(buildMenu({
+  Menu.setApplicationMenu(appMenu.build({
     fullQuit : () => { fullQuit = true; app.quit() },
     fullscreenToggle : () => { mailboxWindow.setFullScreen(!mailboxWindow.isFullScreen()) },
     reload : () => { mailboxWindow.webContents.reload() },
