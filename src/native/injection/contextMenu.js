599feb90428659ddb3d5ad73b253eb96132b8835
Thomas Beverley <tom.beverley101@gmail.com>
2016-01-31 18:31:46 +0000

    More work on spell checking including suggestions to support #19

---
+++ src/native/injection/contextMenu.js
@@ -0,0 +1,87 @@
+;(function () {
+  'use strict'
+
+  const electron = require('electron')
+  const remote = electron.remote
+  const Menu = remote.Menu
+
+  const textOnlyRE = new RegExp(/[^a-z]+/gi)
+
+  class ContextMenu {
+    /* **************************************************************************/
+    // Lifecycle
+    /* **************************************************************************/
+
+    constructor () {
+      document.addEventListener('contextmenu', (evt) => {
+        const x = evt.clientX
+        const y = evt.clientY
+        const selection = window.getSelection()
+        const textSelection = selection.toString()
+        const menu = []
+
+        // Spell check suggestions
+        if (this.__spellcheckProvider__) {
+          if (this.isTexteditorTarget(evt)) {
+            if (textOnlyRE.exec(textSelection.trim()) === null) {
+              const suggestions = this.__spellcheckProvider__.suggest(textSelection.trim())
+              if (suggestions.length) {
+                suggestions.forEach(s => {
+                  menu.push({
+                    label: s,
+                    click: () => {
+                      const range = selection.getRangeAt(0)
+                      range.deleteContents()
+                      range.insertNode(document.createTextNode(s))
+                    }
+                  })
+                })
+                menu.push({ type: 'separator' })
+              }
+            }
+          }
+        }
+
+        // Undo / redo
+        menu.push({ label: 'Undo', role: 'undo' })
+        menu.push({ label: 'Redo', role: 'redo' })
+        menu.push({ type: 'separator' })
+
+        // Text editor / text selection
+        if (this.isTexteditorTarget(evt)) {
+          menu.push({ label: 'Cut', role: 'cut' })
+          menu.push({ label: 'Copy', role: 'copy' })
+          menu.push({ label: 'Paste', role: 'paste' })
+          menu.push({ type: 'separator' })
+        } else if (textSelection) {
+          menu.push({ label: 'Copy', role: 'copy' })
+          menu.push({ type: 'separator' })
+        }
+
+        // Misc
+        menu.push({ label: 'Select all', role: 'selectall' })
+
+        Menu.buildFromTemplate(menu).popup(null, x, y)
+      }, false)
+    }
+
+    /* **************************************************************************/
+    // Properties
+    /* **************************************************************************/
+
+    set spellcheckProvider (v) { this.__spellcheckProvider__ = v }
+
+    /* **************************************************************************/
+    // Utils
+    /* **************************************************************************/
+
+    isTexteditorTarget (evt) {
+      if (evt.target.tagName === 'INPUT' || evt.target.tagName === 'TEXTAREA') { return true }
+      if (evt.path.findIndex(e => e.getAttribute && e.getAttribute('contentEditable') === 'true') !== -1) { return true }
+      return false
+    }
+
+  }
+
+  module.exports = new ContextMenu()
+})()
--- src/native/injection/google.js
+++ src/native/injection/google.js
@@ -1,36 +1,11 @@
-require('./clickReport')
-require('./zoomLevel')
-require('./spellchecker')()
-
-/**
-* Interfaces with the way gmail opens new windows. They do....
-* w = window.open("", "_blank", "")
-* w.document.write('<META HTTP-EQUIV="refresh" content="0; url=https://www.google.com/contacts/u/0">')
-* this proxies that request and sends it up to be opened
-*/
 ;(function () {
   'use strict'
 
-  const ipcRenderer = require('electron').ipcRenderer
-  const defaultfn = window.open
-
-  window.open = function () {
-    if (arguments[0] === '' && arguments[1] === '_blank') {
-      return {
-        document: {
-          write: function (value) {
-            const parser = new window.DOMParser()
-            const xml = parser.parseFromString(value, 'text/xml')
-            if (xml.firstChild && xml.firstChild.getAttribute('HTTP-EQUIV') === 'refresh') {
-              const content = xml.firstChild.getAttribute('content')
-              const url = content.replace('0; url=', '')
-              ipcRenderer.sendToHost({ type: 'js-new-window', url: url })
-            }
-          }
-        }
-      }
-    } else {
-      return defaultfn.apply(this, Array.from(arguments))
-    }
-  }
+  require('./clickReport')
+  require('./zoomLevel')
+  require('./googleWindowOpen')
+  const contextMenu = require('./contextMenu')
+  require('./spellchecker')(provider => {
+    contextMenu.spellcheckProvider = provider
+  })
 })()
