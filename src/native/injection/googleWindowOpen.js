836896935702bb104451d9ac7985426c77df63d3
Thomas Beverley <tom.beverley101@gmail.com>
2016-02-05 21:25:58 +0000

    Fix for printing pdfs #23

--- src/native/injection/googleWindowOpen.js
+++ src/native/injection/googleWindowOpen.js
@@ -8,25 +8,41 @@
   'use strict'
 
   const ipcRenderer = require('electron').ipcRenderer
-  const defaultfn = window.open
 
-  window.open = function () {
-    if (arguments[0] === '' && arguments[1] === '_blank') {
-      return {
-        document: {
-          write: function (value) {
-            const parser = new window.DOMParser()
-            const xml = parser.parseFromString(value, 'text/xml')
-            if (xml.firstChild && xml.firstChild.getAttribute('HTTP-EQUIV') === 'refresh') {
-              const content = xml.firstChild.getAttribute('content')
-              const url = content.replace('0; url=', '')
-              ipcRenderer.sendToHost({ type: 'js-new-window', url: url })
+  const injectWindow = function (w) {
+    const defaultfn = w.open
+
+    w.open = function () {
+      if (arguments[0] === '' && arguments[1] === '_blank') {
+        return {
+          document: {
+            write: function (value) {
+              const parser = new window.DOMParser()
+              const xml = parser.parseFromString(value, 'text/xml')
+              if (xml.firstChild && xml.firstChild.getAttribute('HTTP-EQUIV') === 'refresh') {
+                const content = xml.firstChild.getAttribute('content')
+                const url = content.replace('0; url=', '')
+                ipcRenderer.sendToHost({ type: 'js-new-window', url: url })
+              }
             }
           }
         }
+      } else {
+        return defaultfn.apply(this, Array.from(arguments))
       }
-    } else {
-      return defaultfn.apply(this, Array.from(arguments))
     }
   }
+
+  // Inject into the main window
+  injectWindow(window)
+
+  // Inject into the subframes. This is used for opening pdfs for example
+  document.addEventListener('DOMContentLoaded', function () {
+    const frames = document.querySelectorAll('iframe')
+    for (let i = 0; i < frames.length; i++) {
+      try {
+        injectWindow(frames[i].contentWindow)
+      } catch (ex) { }
+    }
+  }, false)
 })()
