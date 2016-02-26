1321ee3e432ad1a3067c6d8cd34167bc0643e8e1
Sasha Chedygov <sasha1rus@gmail.com>
2016-02-25 17:13:50 -0800

    Fix code style to adhere to StandardJS

--- src/mailbox/ui/App.js
+++ src/mailbox/ui/App.js
@@ -152,7 +152,7 @@ module.exports = React.createClass({
       ])
       this.appTray.setToolTip(unreadText)
       this.appTray.setContextMenu(contextMenu)
-      this.appTray.on('click', function(e) {
+      this.appTray.on('click', function (e) {
         ipc.send('focus-app')
       })
     } else {
--- src/main/WindowManager.js
+++ src/main/WindowManager.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const app = require('app')
-const AppSettings = require('./AppSettings')
 
 class WindowManager {
 
