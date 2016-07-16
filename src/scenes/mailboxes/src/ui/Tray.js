c2ac426f8e71b214ff96715b628ecfbe479f8cc9
Thomas Beverley <tom.beverley101@gmail.com>
2016-07-16 10:18:09 +0100

    Updated tray icon to be much stricter on update clause

--- src/scenes/mailboxes/src/ui/Tray.js
+++ src/scenes/mailboxes/src/ui/Tray.js
@@ -2,7 +2,6 @@ const electron = window.nativeRequire('electron')
 const {Tray, systemPreferences, Menu, nativeImage} = electron.remote
 const ipc = electron.ipcRenderer
 const React = require('react')
-const shallowCompare = require('react-addons-shallow-compare')
 const {mailboxDispatch} = require('../Dispatch')
 const mailboxActions = require('../stores/mailbox/mailboxActions')
 const {
@@ -13,6 +12,7 @@ const {
 module.exports = React.createClass({
   displayName: 'Tray',
 
+  // Pretty strict on updating. If you're chaning these, change shouldComponentUpdate :)
   propTypes: {
     unreadCount: React.PropTypes.number.isRequired,
     unreadMessages: React.PropTypes.object.isRequired,
@@ -20,10 +20,17 @@ module.exports = React.createClass({
   },
 
   /* **************************************************************************/
-  // Lifecycle
+  // Component Lifecycle
   /* **************************************************************************/
 
   componentDidMount () {
+    this.appTray = new Tray(nativeImage.createFromDataURL(BLANK_PNG))
+    if (process.platform === 'win32') {
+      this.appTray.on('double-click', () => {
+        ipc.send('focus-app')
+      })
+    }
+
     const loader = new window.Image()
     loader.src = MAIL_SVG
     loader.onload = (e) => {
@@ -32,9 +39,14 @@ module.exports = React.createClass({
   },
 
   componentWillUnmount () {
-    if (this.state.appTray) {
-      this.state.appTray.destroy()
+    if (this.appTray) {
+      this.appTray.destroy()
+      this.appTray = null
     }
+  },
+
+  componentWillReceiveProps (nextProps) {
+    this.setState(this.generateCtxMenuUnreadMessages(nextProps.unreadMessages))
   },
 
   /* **************************************************************************/
@@ -45,22 +57,81 @@ module.exports = React.createClass({
   getDefaultUnreadColor () { return '#C82018' },
 
   getInitialState () {
-    const appTray = new Tray(nativeImage.createFromDataURL(BLANK_PNG))
-    if (process.platform === 'win32') {
-      appTray.on('double-click', () => {
-        ipc.send('focus-app')
-      })
-    }
-    return { appTray: appTray }
+    return Object.assign(
+      {
+        icon: null
+      },
+      this.generateCtxMenuUnreadMessages(this.props.unreadMessages)
+    )
   },
 
-  shouldComponentUpdate (nextProps, nextState) {
-    return shallowCompare(this, nextProps, nextState)
+  /**
+  * Generates the unread message data for the context menu in a way that it
+  * can be rendered and diffed
+  * @param unreadMessages: the raw unread messages to generate from
+  * @return { ctxMenuUnreadMessages, ctxMenuUnreadMessagesSig } with ctxMenuUnreadMessages containing
+  * the up to date message headers and ctxMenuUnreadMessagesSig containing a string hash of them
+  */
+  generateCtxMenuUnreadMessages (unreadMessages) {
+    const messageInfos = Object.keys(unreadMessages)
+      .reduce((acc, mailboxId) => {
+        const messages = Object.keys(unreadMessages[mailboxId])
+          .map((id) => unreadMessages[mailboxId][id])
+          .map((info) => {
+            info.mailboxId = mailboxId
+            return info
+          })
+        return acc.concat(messages)
+      }, [])
+      .filter((info) => info.message !== undefined)
+      .sort((a, b) => {
+        return parseInt(b.message.internalDate) - parseInt(a.message.internalDate)
+      })
+      .slice(0, 5)
+      .map((info) => {
+        const headers = info.message.payload.headers
+        const subject = (headers.find((h) => h.name === 'Subject') || {}).value || 'No Subject'
+        const fromEmail = (headers.find((h) => h.name === 'From') || {}).value || ''
+        const fromEmailMatch = fromEmail.match('(.+)<(.+)@(.+)>$')
+        if (fromEmailMatch) {
+          info.snippet = fromEmailMatch[1].trim() + ' : ' + subject
+        } else {
+          info.snippet = fromEmail + ' : ' + subject
+        }
+        return info
+      })
+      .map((info) => {
+        return {
+          label: info.snippet,
+          mailboxId: info.mailboxId,
+          threadId: info.message.threadId,
+          messageId: info.message.id,
+          id: `${info.mailboxId}:${info.message.threadId}:${info.message.id}` // used for update tracking
+        }
+      })
+
+    return {
+      ctxMenuUnreadMessages: messageInfos,
+      ctxMenuUnreadMessagesSig: messageInfos.map((info) => info.id).join('|')
+    }
   },
 
   /* **************************************************************************/
   // Rendering
   /* **************************************************************************/
+
+  shouldComponentUpdate (nextProps, nextState) {
+    if (this.props.unreadCount !== nextProps.unreadCount) { return true }
+    if (this.state.ctxMenuUnreadMessagesSig !== nextState.ctxMenuUnreadMessagesSig) { return true }
+    if (!!this.state.icon !== !!nextState.icon) { return true }
+
+    const trayDiff = ['unreadColor', 'readColor', 'showUnreadCount', 'isReadColorDefault'].findIndex((k) => {
+      return this.props.traySettings[k] !== nextProps.traySettings[k]
+    }) !== -1
+    if (trayDiff) { return true }
+
+    return false
+  },
 
   /**
   * @return the nativeImage for the tray
@@ -127,45 +198,16 @@ module.exports = React.createClass({
   * @return the context menu for the tray icon
   */
   renderContextMenu () {
-    const unreadMessages = this.props.unreadMessages
-    // Build the unread items up
-    const unreadItems = Object.keys(unreadMessages)
-      .reduce((acc, mailboxId) => {
-        const messages = Object.keys(unreadMessages[mailboxId])
-          .map((id) => unreadMessages[mailboxId][id])
-          .map((info) => {
-            info.mailboxId = mailboxId
-            return info
-          })
-        return acc.concat(messages)
-      }, [])
-      .filter((info) => info.message !== undefined)
-      .sort((a, b) => {
-        return parseInt(b.message.internalDate) - parseInt(a.message.internalDate)
-      })
-      .slice(0, 5)
-      .map((info) => {
-        const headers = info.message.payload.headers
-        const subject = (headers.find((h) => h.name === 'Subject') || {}).value || 'No Subject'
-        const fromEmail = (headers.find((h) => h.name === 'From') || {}).value || ''
-        const fromEmailMatch = fromEmail.match('(.+)<(.+)@(.+)>$')
-        if (fromEmailMatch) {
-          info.snippet = fromEmailMatch[1].trim() + ' : ' + subject
-        } else {
-          info.snippet = fromEmail + ' : ' + subject
+    const unreadItems = this.state.ctxMenuUnreadMessages.map((info) => {
+      return {
+        label: info.label,
+        click: (e) => {
+          ipc.send('focus-app', { })
+          mailboxActions.changeActive(info.mailboxId)
+          mailboxDispatch.openMessage(info.mailboxId, info.threadId, info.messageId)
         }
-        return info
-      })
-      .map((info) => {
-        return {
-          label: info.snippet,
-          click: (e) => {
-            ipc.send('focus-app', { })
-            mailboxActions.changeActive(info.mailboxId)
-            mailboxDispatch.openMessage(info.mailboxId, info.message.threadId, info.message.id)
-          }
-        }
-      })
+      }
+    })
 
     // Build the template
     let template = [
@@ -186,11 +228,11 @@ module.exports = React.createClass({
   },
 
   render () {
-    if (!this.state.appTray || !this.state.icon) { return false }
-    this.state.appTray.setImage(this.renderImage())
-    this.state.appTray.setToolTip(this.renderTooltip())
-    this.state.appTray.setContextMenu(this.renderContextMenu())
+    if (!this.appTray || !this.state.icon) { return false }
+    this.appTray.setImage(this.renderImage())
+    this.appTray.setToolTip(this.renderTooltip())
+    this.appTray.setContextMenu(this.renderContextMenu())
 
-    return <div></div>
+    return (<div></div>)
   }
 })
