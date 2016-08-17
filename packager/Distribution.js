cf5ec2be6ed6ee8ab40d04b309bd285c58d42632
Thomas Beverley <tom.beverley101@gmail.com>
2016-08-17 22:36:28 +0100

    First attempt at getting deb packaging to work. Not right yet, but a good first start #224

--- packager/Distribution.js
+++ packager/Distribution.js
@@ -3,10 +3,16 @@ const msiPackager = require('msi-packager')
 const path = require('path')
 const fs = require('fs-extra')
 const childProcess = require('child_process')
+const makedeb = require('makedeb')
 const TaskLogger = require('./TaskLogger')
 
 const { ROOT_PATH } = require('./constants')
 const { WINDOWS_UPGRADE_CODE } = require('../src/shared/credentials')
+
+const ARCH = {
+  X86: 'x86',
+  X64: 'x64'
+}
 
 class Distribution {
 
@@ -22,7 +28,7 @@ class Distribution {
 
     return new Promise((resolve, reject) => {
       const task = TaskLogger.start('OSX DMG')
-      const filename = `WMail_${pkg.version.replace(/\./g, '_')}${pkg.prerelease ? '_prerelease' : ''}.dmg`
+      const filename = `WMail_${pkg.version.replace(/\./g, '_')}${pkg.prerelease ? '_prerelease' : ''}_osx.dmg`
       const distPath = path.join(ROOT_PATH, 'dist')
       const targetPath = path.join(distPath, filename)
       fs.mkdirsSync(distPath)
@@ -74,9 +80,9 @@ class Distribution {
       const task = TaskLogger.start(`Windows MSI (${arch})`)
 
       // Pre-calc all the needed paths
-      const filename = `WMail_${pkg.version.replace(/\./g, '_')}${pkg.prerelease ? '_prerelease' : ''}_${arch}`
+      const filename = `WMail_${pkg.version.replace(/\./g, '_')}${pkg.prerelease ? '_prerelease' : ''}_windows_${arch}`
       const distPath = path.join(ROOT_PATH, 'dist')
-      const builtPath = path.join(ROOT_PATH, arch === 'x64' ? 'WMail-win32-x64' : 'WMail-win32-ia32')
+      const builtPath = path.join(ROOT_PATH, arch === ARCH.X64 ? 'WMail-win32-x64' : 'WMail-win32-ia32')
       const msiTargetPath = path.join(distPath, filename + '.msi')
       const zipTargetPath = path.join(distPath, filename + '.zip')
       const zipExtraPaths = [
@@ -127,6 +133,81 @@ class Distribution {
   }
 
   /**
+  * Distributes the app for linux
+  * @param pkg: the package info
+  * @param arch: one of 'x86' or 'x64'
+  * @return promise
+  */
+  static distributeLinuxTar (pkg, arch) {
+    return new Promise((resolve, reject) => {
+      const task = TaskLogger.start(`Linux tar (${arch})`)
+
+      const filename = `WMail_${pkg.version.replace(/\./g, '_')}${pkg.prerelease ? '_prerelease' : ''}_linux_${arch}.tar.gz`
+      const targetPath = path.join(ROOT_PATH, 'dist', filename)
+      const builtDirectory = arch === ARCH.X64 ? 'WMail-linux-x64' : 'WMail-linux-ia32'
+
+      try {
+        fs.unlinkSync(targetPath)
+      } catch (ex) { /* no-op */ }
+
+      const cmd = `cd ${ROOT_PATH}; tar czf "${targetPath}" "${builtDirectory}"`
+      childProcess.exec(cmd, {}, (error, stdout, stderr) => {
+        if (error) { console.error(error) }
+        if (stdout) { console.log(`stdout: ${stdout}`) }
+        if (stderr) { console.log(`stderr: ${stderr}`) }
+
+        if (error) { task.fail(); reject(); return }
+
+        task.finish()
+        resolve()
+      })
+    })
+  }
+
+  /**
+  * Distributes the deb for linux
+  * @param pkg: the package info
+  * @param arch: one of 'x86' or 'x64'
+  * @return promise
+  */
+  static distributeLinuxDeb (pkg, arch) {
+    return new Promise((resolve, reject) => {
+      const task = TaskLogger.start(`Linux deb (${arch})`)
+
+      const packageName = `WMail-${pkg.version.replace(/\./g, '-')}${pkg.prerelease ? '-prerelease' : ''}-linux-${arch}`
+      const distPath = path.join(ROOT_PATH, 'dist')
+      const builtPath = path.join(ROOT_PATH, arch === ARCH.X64 ? 'WMail-linux-x64' : 'WMail-linux-ia32')
+
+      let debArch = 'i386'
+      switch (arch) {
+        case ARCH.X86: debArch = 'i386'; break
+        case ARCH.X64: debArch = 'x86_64'; break
+      }
+
+      makedeb({
+        packageName: packageName,
+        version: pkg.version,
+        buildDir: builtPath,
+        outDir: distPath,
+        installPath: '/opt/wmail',
+        overwrite: false,
+        maintainer: 'https://github.com/Thomas101/wmail',
+        section: 'main',
+        priority: 'optional',
+        architecture: debArch,
+        essential: 'no',
+        packageDescription: ''
+      }).then(() => {
+        task.finish()
+        resolve()
+      }, (err) => {
+        task.fail()
+        reject(err)
+      })
+    })
+  }
+
+  /**
   * Distributes the app for the given platforms
   * @param platforms: the platforms to distribute for
   * @param pkg: the package info
@@ -138,8 +219,14 @@ class Distribution {
         return acc.then(() => Distribution.distributeDarwin(pkg))
       } else if (platform === 'win32') {
         return acc
-          .then(() => Distribution.distributeWindows(pkg, 'x86'))
-          .then(() => Distribution.distributeWindows(pkg, 'x64'))
+          .then(() => Distribution.distributeWindows(pkg, ARCH.X86))
+          .then(() => Distribution.distributeWindows(pkg, ARCH.X64))
+      } else if (platform === 'linux') {
+        return acc
+          .then(() => Distribution.distributeLinuxTar(pkg, ARCH.X86))
+          .then(() => Distribution.distributeLinuxDeb(pkg, ARCH.X86))
+          .then(() => Distribution.distributeLinuxTar(pkg, ARCH.X64))
+          .then(() => Distribution.distributeLinuxDeb(pkg, ARCH.X64))
       } else {
         return acc
       }
