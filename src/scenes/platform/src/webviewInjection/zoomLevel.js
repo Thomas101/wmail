512eb366fd4c7fbedf8ae2f348f4a4c3646d05ec
Thomas Beverley <tom.beverley101@gmail.com>
2016-08-24 19:24:13 +0100

    Upgraded libs. Fix for ever-resetting zoom factor #257

--- src/scenes/platform/src/webviewInjection/zoomLevel.js
+++ src/scenes/platform/src/webviewInjection/zoomLevel.js
@@ -6,6 +6,8 @@
   const ipc = electron.ipcRenderer
 
   ipc.on('zoom-factor-set', (evt, data) => {
-    webFrame.setZoomFactor(data.value)
+    // Cant set zoom factor due to https://github.com/electron/electron/issues/6958. Nice workaround below...
+    // webFrame.setZoomFactor(data.value)
+    webFrame.setZoomLevelLimits(data.value, data.value)
   })
 })()
