111a184a4a61993099f2ff53fb6eaa037aa573f9
Thomas Beverley <tom.beverley101@gmail.com>
2016-08-28 13:26:05 +0100

    Removed some unused packaging code

--- packager/Distribution.js
+++ packager/Distribution.js
@@ -3,7 +3,6 @@ const msiPackager = require('msi-packager')
 const path = require('path')
 const fs = require('fs-extra')
 const childProcess = require('child_process')
-const makedeb = require('makedeb')
 const TaskLogger = require('./TaskLogger')
 
 const { ROOT_PATH } = require('./constants')
@@ -165,49 +164,6 @@ class Distribution {
   }
 
   /**
-  * Distributes the deb for linux
-  * @param pkg: the package info
-  * @param arch: one of 'x86' or 'x64'
-  * @return promise
-  */
-  static distributeLinuxDeb (pkg, arch) {
-    return new Promise((resolve, reject) => {
-      const task = TaskLogger.start(`Linux deb (${arch})`)
-
-      const packageName = `WMail-${pkg.version.replace(/\./g, '-')}${pkg.prerelease ? '-prerelease' : ''}-linux-${arch}`
-      const distPath = path.join(ROOT_PATH, 'dist')
-      const builtPath = path.join(ROOT_PATH, arch === ARCH.X64 ? 'WMail-linux-x64' : 'WMail-linux-ia32')
-
-      let debArch = 'i386'
-      switch (arch) {
-        case ARCH.X86: debArch = 'i386'; break
-        case ARCH.X64: debArch = 'x86_64'; break
-      }
-
-      makedeb({
-        packageName: packageName,
-        version: pkg.version,
-        buildDir: builtPath,
-        outDir: distPath,
-        installPath: '/opt/wmail',
-        overwrite: false,
-        maintainer: 'https://github.com/Thomas101/wmail',
-        section: 'main',
-        priority: 'optional',
-        architecture: debArch,
-        essential: 'no',
-        packageDescription: ''
-      }).then(() => {
-        task.finish()
-        resolve()
-      }, (err) => {
-        task.fail()
-        reject(err)
-      })
-    })
-  }
-
-  /**
   * Distributes the app for the given platforms
   * @param platforms: the platforms to distribute for
   * @param pkg: the package info
@@ -225,8 +181,6 @@ class Distribution {
         return acc
           .then(() => Distribution.distributeLinuxTar(pkg, ARCH.X86))
           .then(() => Distribution.distributeLinuxTar(pkg, ARCH.X64))
-          // .then(() => Distribution.distributeLinuxDeb(pkg, ARCH.X86))
-          // .then(() => Distribution.distributeLinuxDeb(pkg, ARCH.X64))
       } else {
         return acc
       }
