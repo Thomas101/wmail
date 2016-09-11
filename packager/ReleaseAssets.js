06a16c0902484de0c12219ebcfd32f5ed95c8d47
Thomas Beverley <tom.beverley101@gmail.com>
2016-09-11 10:33:10 +0100

    Fix copying icon in packager

--- packager/ReleaseAssets.js
+++ packager/ReleaseAssets.js
@@ -13,7 +13,8 @@ class ReleaseAssets {
     const platformsSet = new Set(platforms)
     if (platformsSet.has('darwin')) {
       fs.copySync(path.join(__dirname, 'dmg/First Run.html'), path.join(ROOT_PATH, 'WMail-darwin-x64/First Run.html'))
-    } else if (platformsSet.has('linux')) {
+    }
+    if (platformsSet.has('linux')) {
       fs.copySync(path.join(ROOT_PATH, 'assets/icons/app.png'), path.join(ROOT_PATH, 'WMail-linux-ia32/icon.png'))
       fs.copySync(path.join(ROOT_PATH, 'assets/icons/app.png'), path.join(ROOT_PATH, 'WMail-linux-x64/icon.png'))
     }
