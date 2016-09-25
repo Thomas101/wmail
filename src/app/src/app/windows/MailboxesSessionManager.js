b4c97d39535ee8ddf4474367648024af9c6b2dbd
Thomas Beverley <tom.beverley101@gmail.com>
2016-09-25 07:55:02 +0100

    Handle the downloads in an entirely custom way to ensure the user maintains file extension and to have some kind of download progress on the filesystem level. #328 #320

--- src/app/src/app/windows/MailboxesSessionManager.js
+++ src/app/src/app/windows/MailboxesSessionManager.js
@@ -1,9 +1,10 @@
-const electron = require('electron')
+const {session, dialog, app} = require('electron')
 const uuid = require('uuid')
 const fs = require('fs-extra')
 const path = require('path')
 const settingStore = require('../stores/settingStore')
 const mailboxStore = require('../stores/mailboxStore')
+const unusedFilename = require('unused-filename')
 
 const COOKIE_PERSIST_PERIOD = 1000 * 30 // 30 secs
 
@@ -47,8 +48,8 @@ class MailboxesSessionManager {
   startManagingSession (partition) {
     if (this.__managed__.has(partition)) { return }
 
-    const ses = electron.session.fromPartition(partition)
-    ses.setDownloadPath(electron.app.getPath('downloads'))
+    const ses = session.fromPartition(partition)
+    ses.setDownloadPath(app.getPath('downloads'))
     ses.on('will-download', (evt, item) => this.handleDownload(evt, item))
     ses.setPermissionRequestHandler(this.handlePermissionRequest)
     ses.webRequest.onCompleted((evt) => this.handleRequestCompleted(evt, ses, partition))
@@ -79,36 +80,48 @@ class MailboxesSessionManager {
   /* ****************************************************************************/
 
   handleDownload (evt, item) {
-    // If the user has chosen - auto save the item
+    // Find out where to save the file
+    let savePath
     if (!settingStore.os.alwaysAskDownloadLocation && settingStore.os.defaultDownloadLocation) {
       const folderLocation = settingStore.os.defaultDownloadLocation
-      const fpath = path.parse(item.getFilename() || 'untitled')
 
-      // Check the file exists
+      // Check the containing folder exists
       fs.ensureDirSync(folderLocation)
+      savePath = unusedFilename.sync(path.join(folderLocation, item.getFilename()))
+    } else {
+      let pickedSavePath = dialog.showSaveDialog(this.mailboxWindow.window, {
+        title: 'Download',
+        defaultPath: path.join(app.getPath('downloads'), item.getFilename())
+      })
 
-      // Keep trying a different variation of the filename until we find one that isn't in use
-      let iter = 0
-      while (true) {
-        const testName = fpath.name + (iter === 0 ? '' : ' (' + iter + ')') + fpath.ext
-        const testPath = path.join(folderLocation, testName)
+      // There's a bit of a pickle here. Whilst asking the user where to save
+      // they may have omitted the file extension. At the same time they may chosen
+      // a filename that is already taken. We don't have any in-built ui to handle
+      // this so the least destructive way is to find a filename that is not
+      // in use and just save to there. In any case if the user picks a path and
+      // that file does already exist we should remove it
+      if (pickedSavePath) {
+        // Remove existing file - save dialog prompts before allowing user to choose pre-existing name
+        try { fs.removeSync(pickedSavePath) } catch (ex) { /* no-op */ }
 
-        let exists
-        try {
-          fs.statSync(testPath)
-          exists = true
-        } catch (ex) {
-          exists = false
+        // User didn't add file extension
+        if (!path.extname(pickedSavePath)) {
+          pickedSavePath += path.extname(item.getFilename())
+          pickedSavePath = unusedFilename.sync(pickedSavePath)
         }
-
-        if (exists) {
-          iter++
-        } else {
-          item.setSavePath(testPath)
-          break
-        }
+        savePath = pickedSavePath
       }
     }
+
+    // Check we still want to save
+    if (!savePath) {
+      item.cancel()
+      return
+    }
+
+    // Set the save - will prevent dialog showing up
+    const downloadPath = unusedFilename.sync(savePath + '.wmaildownload') // just-in-case
+    item.setSavePath(downloadPath)
 
     // Report the progress to the window to display it
     const totalBytes = item.getTotalBytes()
@@ -117,11 +130,18 @@ class MailboxesSessionManager {
       this.updateDownloadProgress(id, item.getReceivedBytes(), totalBytes)
     })
     item.on('done', (e, state) => {
-      this.downloadFinished(id)
       if (state === 'completed') {
-        const savePath = item.getSavePath()
-        const saveName = path.basename(savePath)
-        this.mailboxWindow.downloadCompleted(savePath, saveName)
+        // Download item will get destroyed before move callback completes. If
+        // you need any info from it grab it before calling fs.move
+        fs.move(downloadPath, savePath, () => {
+          this.downloadFinished(id)
+          const saveName = path.basename(savePath)
+          this.mailboxWindow.downloadCompleted(savePath, saveName)
+        })
+      } else {
+        // Tidy-up on failure
+        try { fs.removeSync(downloadPath) } catch (ex) { /* no-op */ }
+        this.downloadFinished(id)
       }
     })
   }
