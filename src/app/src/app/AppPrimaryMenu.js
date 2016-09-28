be6bb0f5d3ca6968b91ceda0b3f3a90b06a7e0e8
Thomas Beverley <tom.beverley101@gmail.com>
2016-09-28 17:54:56 +0100

    Changed depricated selector: to role in appMenu to close #335

--- src/app/src/app/AppPrimaryMenu.js
+++ src/app/src/app/AppPrimaryMenu.js
@@ -48,14 +48,14 @@ class AppPrimaryMenu {
       {
         label: 'Edit',
         submenu: [
-          { label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:' },
-          { label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', selector: 'redo:' },
+          { label: 'Undo', accelerator: 'CmdOrCtrl+Z', role: 'undo' },
+          { label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', role: 'redo' },
           { type: 'separator' },
-          { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
-          { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
-          { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
-          { label: 'Paste and match style', accelerator: 'CmdOrCtrl+Shift+V', selector: 'pasteAndMatchStyle:' },
-          { label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:' },
+          { label: 'Cut', accelerator: 'CmdOrCtrl+X', role: 'cut' },
+          { label: 'Copy', accelerator: 'CmdOrCtrl+C', role: 'copy' },
+          { label: 'Paste', accelerator: 'CmdOrCtrl+V', role: 'paste' },
+          { label: 'Paste and match style', accelerator: 'CmdOrCtrl+Shift+V', role: 'pasteAndMatchStyle' },
+          { label: 'Select All', accelerator: 'CmdOrCtrl+A', role: 'selectAll' },
           { type: 'separator' },
           { label: 'Find', accelerator: 'CmdOrCtrl+F', click: this._selectors.find },
           { label: 'Find Next', accelerator: 'CmdOrCtrl+G', click: this._selectors.findNext }
